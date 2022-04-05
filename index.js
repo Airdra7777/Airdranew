9080c32d51da0392d6ccee87b04d4d7fee5252ab
Airdra7777 <103041807+Airdra7777@users.noreply.github.com>
2022-04-05 17:46:55 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+Bump jormungandr-automation from `e8c4bca` to `45ccdc2`
+Bumps [jormungandr-automation](https://github.com/input-output-hk/jormungandr) from `e8c4bca` to `45ccdc2`.
+- [Release notes](https://github.com/input-output-hk/jormungandr/releases)
+- [Commits](https://github.com/input-output-hk/jormungandr/compare/e8c4bca46527798adb631dff7d99605115af97c2...45ccdc2a45c6a71d857cbc5542f8afed11b89253)
+
+---
+updated-dependencies:
+- dependency-name: jormungandr-automation
+  dependency-type: direct:production
